a601adb4cbef45dff2fa1f18c539ec31eb5b024e
Ahmed <ahmed01033774549@gmail.com>
2023-03-16 20:12:29 +0200

    f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Description: A C program that prints with printf function.
  *
  * Return Always 0 (Success)
@@ -12,4 +12,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n"};
 	return (0):
-}				
+}
