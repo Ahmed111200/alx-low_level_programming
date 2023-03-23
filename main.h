59ee2681cba1a8aacdc2f6beeedf527a996b08b4
Ahmed <ahmed01033774549@gmail.com>
2023-03-23 19:41:57 +0200

    f

---
+++ main.h
@@ -0,0 +1,12 @@
+#infndef MAIN_H
+
+#define MAIN_h
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int comvert_day(int month, int day, int year);
+void Print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
