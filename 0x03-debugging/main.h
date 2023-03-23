221de66c635600a88d979283a556286e92970f17
Ahmed <ahmed01033774549@gmail.com>
2023-03-23 20:02:59 +0200

    f

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,12 @@
-nclude <stdio.h>
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-int comvert_day(int month, int day, int year);
-void Print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_renmaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
