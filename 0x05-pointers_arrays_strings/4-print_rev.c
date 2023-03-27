9e5eb9ef7a0b9bcf84d628675359560b24ec1c94
Ahmed <ahmed01033774549@gmail.com>
2023-03-28 00:42:20 +0200

    f

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * print_rev - prints rovorsed string, followed by a new line
+ * print_rev - prints rovorsed string, followod by a new line
  * @s: pointer to the string to print
  * Return: void
 */
-
 
 void print_rev(char *s)
 {
 	int i = 0;
 
 	while (s[i])
-		i++
-
+		i++;
+	
 	while (i--)
 	{
 		_putchar(s[i]);
